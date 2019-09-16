d9c85b4dec56d0d81af5e33526c06caafd83b8a3
Jim Ewald <jewald@parallax.com>
2019-09-16 16:57:50 -0700

    Move unused files to the deprecated folder.

--- src/deprecated/shared-editor-demo.js
+++ src/deprecated/shared-editor-demo.js
@@ -27,50 +27,28 @@
  */
 
 
+// TODO: This file is not referenced anywhere within the Solo project
+
+
 var baseUrl = $("meta[name=base]").attr("content");
 
-var projectData = null;
+
+var projectData = {
+    'board': 'activity-board'
+};
 var ready = false;
 var projectLoaded = false;
-
-var idProject = 0;
+var client_url = undefined;
 
 $(document).ready(function () {
-    projectData = window.data;
+    projectData = data;
     showInfo(projectData);
     projectLoaded = true;
     if (ready) {
         window.frames["content_blocks"].setProfile(projectData['board']);
         window.frames["content_blocks"].init(projectData['board'], []);
     }
-
-    $('#save-project').on('click', function () {
-        saveProject();
-    });
-
 });
-
-showInfo = function (data) {
-    if (getURLParameter('debug')) console.log(data);
-    $(".project-name").text(data['name']);
-    if (!data['yours']) {
-        $(".project-owner").text("(" + data['user'] + ")");
-    }
-};
-
-saveProject = function () {
-    var code = window.frames["content_blocks"].getXml();
-    projectData['code'] = code;
-    $.post(baseUrl + 'rest/project/code', projectData, function (data) {
-        var previousOwner = projectData['yours'];
-        projectData = data;
-        projectData['code'] = code; // Save code in projectdata to be able to verify if code has changed upon leave
-        utils.showMessage("Project saved", "The project has been saved");
-        if (!previousOwner) {
-            window.location.href = baseUrl + 'projecteditor?id=' + data['id'];
-        }
-    });
-};
 
 blocklyReady = function () {
     if (projectLoaded) {
@@ -81,37 +59,22 @@ blocklyReady = function () {
     }
 };
 
+showInfo = function (data) {
+    $(".project-name").text(data['name']);
+    if (!data['yours']) {
+        $(".project-owner").text("(" + data['user'] + ")");
+    }
+}
+
 loadProject = function () {
-    if (projectData !== null) {
+    if (projectData['code']) {
         window.frames["content_blocks"].load(projectData['code']);
     }
 };
 
 window.onbeforeunload = function () {
-    if (checkLeave()) {
-        return "The project has been changed since the last save.";
-    }
-};
-
-checkLeave = function () {
     var currentXml = window.frames["content_blocks"].getXml();
-    //console.log(projectData['code']);
-    //console.log(currentXml);
-    if (projectData === null) {
-        if (currentXml === '<xml xmlns="http://www.w3.org/1999/xhtml"></xml>') {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        if (projectData['code'] === currentXml) {
-            return false;
-        } else {
-            return true;
-        }
+    if (currentXml !== '<xml xmlns="http://www.w3.org/1999/xhtml"></xml>') {
+        // TODO Save in localstorage
     }
 };
-
-setInterval(function () {
-    $.get(baseUrl + 'ping');
-}, 60000);
