633d3dbf348ed0e6853fb049ea0359e7d72ba476
Jim Ewald <jewald@parallax.com>
2020-08-12 15:31:26 -0700

    Update application version to 1.5.3.

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -44,7 +44,7 @@ export const EnableSentry = true;
  *     {b#} is the beta release number.
  *     {rc#} is the release candidate number.
  */
-export const APP_VERSION = '1.5.2';
+export const APP_VERSION = '1.5.3';
 
 /**
  * Constant string that represents the base, empty project header
