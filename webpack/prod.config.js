6e46fff292b7a80aa53c136d572aba556162c23f
Jim Ewald <jewald@parallax.com>
2021-07-28 12:33:05 -0700

    Update Sass for local install and build

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -87,15 +87,27 @@ module.exports = (opts) => {
     module: {
       rules: [
         {
-          test: /\.css$/,
-          include: [
-            path.resolve(__dirname, '../sass')
-          ],
+          test: /\.s[ac]ss$/i,
           use: [
-            'style-loader',
-            'css-loader'
-          ]
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
         },
+        //
+        // {
+        //   test: /\.css$/,
+        //   include: [
+        //     path.resolve(__dirname, '../sass')
+        //   ],
+        //   use: [
+        //     'style-loader',
+        //     'css-loader'
+        //   ]
+        // },
       ]
     },
     plugins: [
@@ -112,11 +124,6 @@ module.exports = (opts) => {
         chunks: ["index"],
         filename: 'index.html',
       }),
-      // new HtmlWebpack({
-      //   template: './src/templates/editor.html',
-      //   chunks: ["editor"],
-      //   filename: 'blocklyc.html',
-      // }),
       new CopyPlugin({
         patterns: [
           {from: path.resolve(__dirname, blocklyMedia), to: path.resolve(__dirname, `${targetPath}/media`)},
