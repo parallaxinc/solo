7f542681aa91d6613d5f6c85cbb79a07ff51611d
Jim Ewald <jewald@parallax.com>
2021-08-05 17:22:01 -0700

    Refacor experimental code into separate modules. Config webpack to exclude experimental modules.

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -65,7 +65,10 @@ module.exports = (opts) => {
         './src/scss',
         './node_modules',
       ],
-      extensions: ['.js', '.scss']
+      extensions: [
+        '.js',
+        '.scss'
+      ],
     },
     optimization: {
       splitChunks: {
@@ -109,7 +112,13 @@ module.exports = (opts) => {
             },
           ],
         },
-
+        {
+          test: /\.js$/,
+          exclude: [
+            path.resolve(__dirname, './src/modules/blockly/generators/propc/comms/lcd_parallel.js'),
+            path.resolve(__dirname, './src/modules/blockly/generators/propc/comms/wx_simple.js'),
+          ]
+        }
       ]
     },
     plugins: [
