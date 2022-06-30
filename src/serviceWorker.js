cbc79db4f8d6537854c3d6f9f458b831f0416241
Jim Ewald <jewald@parallax.com>
2022-06-30 09:53:55 -0700

    Implement root service worker.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -20,16 +20,114 @@
  *   DEALINGS IN THE SOFTWARE.
  */
 
-self.addEventListener('install', (event) => {
-  const cacheKey = 'MyFancyCacheName_v1'
+// import {Workbox} from 'workbox-window';
 
-  event.waitUntil(caches.open(cacheKey).then((cache) => {
-    // Add all the assets in the array to the 'MyFancyCacheName_v1'
-    // `Cache` instance for later use.
-    return cache.addAll([
-      '/bootstrap.min.css',
-      '/main.css'
-    ])
-  }))
-})
+// import { CacheableResponsePlugin } from "workbox-cacheable-response";
+// import { ExpirationPlugin } from "workbox-expiration";
+// import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from "workbox-precaching";
+// import { registerRoute, NavigationRoute } from "workbox-routing";
+// import { NetworkFirst, NetworkOnly, StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
+import {precacheAndRoute} from 'workbox-precaching';
+
+// Enable debug mode during development
+console.log(`Hostname: ${location.hostname}`)
+const DEBUG_MODE = location.hostname.endsWith(".app.local") || location.hostname === "localhost";
+
+const cacheName = 'SoloCache_V1';
+const SW_VERSION = '1.0.0';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+// const divInstall = document.getElementById('installContainer');
+// const butInstall = document.getElementById('butInstall');
+
+// workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+  // self.addEventListener('install', (event) => {
+  //   event.waitUntil(caches.open(cacheName).then((cache) => {
+  //     console.log(`Adding items to the ${cacheName} cache`)
+  //
+  //     // Add all the assets in the array to the 'MyFancyCacheName_v1'
+  //     // `Cache` instance for later use.
+  //     return cache.addAll([
+  //       // CSS
+  //       '/bootstrap.min.css',
+  //       '/main.css',
+  //
+  //       // Media files
+  //       '/media/1x1.gif',
+  //       '/media/click.mp3',
+  //       '/media/click.ogg',
+  //       'media/click.wav',
+  //       'media/delete.mp3'
+  //     ]);
+  //   }));
+  // });
+
+  self.addEventListener('fetch', async (event) => {
+    // Is this a request for an image?
+    if (event.request.destination === 'image') {
+      // Open the cache
+      event.respondWith(caches.open(cacheName).then((cache) => {
+        // Respond with the image from the cache or from the network
+        return cache.match(event.request).then((cachedResponse) => {
+          return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+            // Add the network response to the cache for future visits.
+            // Note: we need to make a copy of the response to save it in
+            // the cache and use the original as the request response.
+            cache.put(event.request, fetchedResponse.clone());
+
+            // Return the network response
+            return fetchedResponse;
+          });
+        });
+      }));
+      // } else {
+      //   return;
+    }
+  });
+
+
+  /**
+   * Root-level service worker
+   */
+  // const initServiceWorker = () => {
+  //   if ('serviceWorker' in navigator) {
+  //     // Add listener for PWA installation prompts
+  //     pwaBeforeInstall();
+  //
+  //     console.log(`Preparing to load service worker`);
+  //     // Register the service worker
+  //     window.addEventListener('load', () => {
+  //       console.log(`Launching service worker`);
+  //       navigator.serviceWorker.register('/serviceWorker.js')
+  //           .then((registration) => {
+  //             console.log(`Service worker is registered in scope: ${registration.scope}`);
+  //           })
+  //           .catch((err) => {
+  //             console.log(`Service worker not loaded: ${err.message}`);
+  //           });
+  //     });
+  //   } else {
+  //     console.log('Service Worker is not supported.');
+  //   }
+  // }
+  //
+  // const pwaBeforeInstall = () => {
+  //   window.addEventListener('beforeinstallprompt', (event) => {
+  //     // Prevent the mini-infobar from appearing on mobile.
+  //     event.preventDefault();
+  //     console.log('👍', 'beforeinstallprompt', event);
+  //     // Stash the event so it can be triggered later.
+  //     window.deferredPrompt = event;
+  //     // Remove the 'hidden' class from the 'install' button container.
+  //     divInstall.classList.toggle('hidden', false);
+  //   });
+  // }
+
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'GET_VERSION') {
+    event.ports[0].postMessage(SW_VERSION);
+  }
+});
 
